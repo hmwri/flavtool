2d6e921dc97c6030f2caa67fa37cf29a0d72b719
taiyuHonma <31509007+hmwri@users.noreply.github.com>
2023-11-12 17:33:06 +0900

    a
    a

---
+++ TsMovieComposer/analyzer/__init__.py
@@ -0,0 +1,2 @@
+from analyze import analyze
+from FlavMp4 import FlavMP4
--- TsMovieComposer/composer.py
+++ TsMovieComposer/composer.py
@@ -8,6 +8,7 @@ from TsMovieComposer.utils.sample_table_creator import SampleTableCreator
 from TsMovieComposer.utils.track_box_creator import TrackBoxCreator
 from TsMovieComposer.logger import logger
 from TsMovieComposer.codec import get_encoder
+from TsMovieComposer.analyzer import FlavMp4, analyze
 
 media_types = Literal["tast", "soun", "vide", "scnt"]
 
@@ -37,54 +38,56 @@ class Composer:
             パースされたMP4データ
         streaming : bool
             strea
+
         """
+        self.flav_mp4 = analyze(parsed_box)
 
-        self.tracks: dict[media_types, TrackComponent | None] = {
-            "tast": None,
-            "soun": None,
-            "vide": None,
-            "scnt": None,
-        }
-
-        self.media_datas: dict[media_types, MediaData | None] = {
-            "tast": None,
-            "soun": None,
-            "vide": None,
-            "scnt": None,
-        }
-
-
-        self.parsed: ContainerBox = parsed_box
-
-        mdat = self.parsed["mdat"]
-        if not isinstance(mdat, MdatBox):
-            raise Exception("mdat parse error")
-        self.mdat : MdatBox = mdat
-
-        mov_header = self.parsed["moov"]["mvhd"]
-        if not isinstance(mov_header, MvhdBox):
-            raise Exception("mov header parse error")
-        self.mov_header = mov_header
-
-
-        for box in self.parsed["moov"].children:
-            if box.box_type == "trak":
-                handler_box = box["mdia"]["hdlr"]
-                if not isinstance(handler_box, HdlrBox):
-                    logger.error("handler box parse error, this track will be ignored. ")
-                    continue
-                subtype: str = handler_box.component_subtype
-                if subtype not in subtype:
-                    logger.warning(f"{subtype} is not supported media type, so it will be ignored.")
-                    continue
-
-                subtype: media_types
-                self.tracks[subtype] = TrackComponent(box)
-                self.media_datas[subtype] = MediaData.from_mdat_box(
-                    self.mdat,
-                    self.sample_tables[subtype],
-                    subtype,
-                    streaming=streaming
+        # self.tracks: dict[media_types, TrackComponent | None] = {
+        #     "tast": None,
+        #     "soun": None,
+        #     "vide": None,
+        #     "scnt": None,
+        # }
+        #
+        # self.media_datas: dict[media_types, MediaData | None] = {
+        #     "tast": None,
+        #     "soun": None,
+        #     "vide": None,
+        #     "scnt": None,
+        # }
+        #
+        #
+        # self.parsed: ContainerBox = parsed_box
+        #
+        # mdat = self.parsed["mdat"]
+        # if not isinstance(mdat, MdatBox):
+        #     raise Exception("mdat parse error")
+        # self.mdat : MdatBox = mdat
+        #
+        # mov_header = self.parsed["moov"]["mvhd"]
+        # if not isinstance(mov_header, MvhdBox):
+        #     raise Exception("mov header parse error")
+        # self.mov_header = mov_header
+        #
+        #
+        # for box in self.parsed["moov"].children:
+        #     if box.box_type == "trak":
+        #         handler_box = box["mdia"]["hdlr"]
+        #         if not isinstance(handler_box, HdlrBox):
+        #             logger.error("handler box parse error, this track will be ignored. ")
+        #             continue
+        #         subtype: str = handler_box.component_subtype
+        #         if subtype not in subtype:
+        #             logger.warning(f"{subtype} is not supported media type, so it will be ignored.")
+        #             continue
+        #
+        #         subtype: media_types
+        #         self.tracks[subtype] = TrackComponent(box)
+        #         self.media_datas[subtype] = MediaData.from_mdat_box(
+        #             self.mdat,
+        #             self.sample_tables[subtype],
+        #             subtype,
+        #             streaming=streaming
                 )
 
 
